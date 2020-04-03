e95fa0948e507441f93c0da82b054526e174e03c
tuominentj <tuominentj-node-prime>
2020-04-03 20:49:54 +0300

    commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-
 //paikallisesti 8080-portti, PORT isolla niin toimii serverillä
 const port = process.env.PORT || 8080;
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -9,6 +9,7 @@ const material_controller = require('./material_controller');
 
 //npm init
 //npm install express
+//npm install mongoose
 //npm install nodemon --save-dev
 //npn run start-dev
 
@@ -39,5 +40,23 @@ app.get("/api/materials", material_controller.api_get_materials);
 // update
 
 // delete
+app.delete("/api/material/:id", material_controller.api_delete_material);
 
-app.listen(port);
+// npm install mongoose
+//"mongodb+srv://admin:Bzxwbu5l5P9aDFhi@cluster0-iklya.mongodb.net/test?retryWrites=true&w=majority" /test? alikansio databasessa
+//muutettiin materialdb:ksi
+const database_uri = "mongodb+srv://admin:Bzxwbu5l5P9aDFhi@cluster0-iklya.mongodb.net/materialdb?retryWrites=true&w=majority";
+// Bzxwbu5l5P9aDFhi
+mongoose.connect(database_uri, {
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+}).then( ()=>{
+    console.log('database connected');
+    app.listen(port);
+}).catch(err=> {
+    console.log(err);
+});
+
+//app.listen(port);
--- material_controller.js
+++ material_controller.js
@@ -1,18 +1,49 @@
-
+const material_model = require('./material_model');
 
 
 // create
+const api_post_material = (req, res, next)=>{
+    let data = {
+        name: req.body.name,
+        min_density: req.body.min_density,
+        max_density: req.body.max_density,
+        min_strength: req.body.min_strength,
+        max_strength: req.body.max_strength,
+        min_strength_density: req.body.min_strength / req.body.max_density,
+        max_strength_density: req.body.max_strength / req.body.min_density
+    };
 
-const api_post_material = (req,res,next)=>{
-    let data = req.body;
-    console.log(data);
-    res.send(JSON.stringify(data));
+    let new_material = material_model(data);
+
+    new_material.save().then(()=>{
+        console.log(new_material);
+        res.send(JSON.stringify(new_material));
+    }).catch(err => {
+        res.status(500);
+        //välitetään virhetieto myös postmanille
+        res.send(err.errmsg);
+        console.log(err);
+    });
+
+    //console.log(data);
+    //alla oleva lähettää ja päättää lähetyksen samalla rivillä
+    //res.send(JSON.stringify(data));
 };
 
 // read
-
 const api_get_materials = (req, res, next)=>{
-    res.send(JSON.stringify([]));
+    //console.log('api_get_materials');
+    material_model.find({})
+    .lean()
+    .then(materials => {
+        res.send(JSON.stringify(materials));
+    }).catch(err => {
+        res.status(500);
+        res.send(err.errmsg);
+        console.log(err);
+    });
+
+    //res.send(JSON.stringify([]));
 };
 
 // update
@@ -20,8 +51,19 @@ const api_get_materials = (req, res, next)=>{
 
 // delete
 
+//delete /api/material/5e87739680a811535c242661
+const api_delete_material = (req, res, next) => {
+    let id = req.params.id;
+    material_model.findByIdAndRemove(id).then(() => {
+        res.send();
+    }).catch(err => {
+        res.status(500);
+        res.send(err.errmsg);
+        console.log(err);
+    });
+};
 
 // exports
 module.exports.api_post_material = api_post_material;
 module.exports.api_get_materials = api_get_materials;
-
+module.exports.api_delete_material = api_delete_material;
